5afaae18356fb3bf491ebb429b72db04357c7475
Sukrit Jaidee <41770170+SukritJaidee@users.noreply.github.com>
2023-02-20 19:05:58 +0700

    Update initial_f.py

--- helper/initial_f.py
+++ helper/initial_f.py
@@ -5,33 +5,33 @@ ran_url = ['https://energysolutions.egat.co.th/karin1', 'https://energysolutions
 rooms = {'karin236':'karin236', 'karin237':'karin237', 'karin238':'karin238', 'karin239':'karin239', 'karin240':'karin240'}
 home_data = {
       'karin236': {
-      "ACCESS_ID":"adp4t43j3ypdcjt8gss5",
-      "ACCESS_KEY":"84c2083744534223b5acf2977fd762f2",
+      "ACCESS_ID":"....................",
+      "ACCESS_KEY":"....................",
       "API_ENDPOINT": "https://openapi.tuyaus.com/",
-      "device_id": "ebe55e1d5cae9be2cdskfl"
+      "device_id": "...................."
       },
       'karin237':{
-      "ACCESS_ID": "fgytkjcqhwjpxueu3qan",
-      "ACCESS_KEY": "6995aeb3128b423cb7a2ce87227f39a2",
+      "ACCESS_ID": "....................",
+      "ACCESS_KEY": "....................",
       "API_ENDPOINT": "https://openapi.tuyaus.com/",
-      "device_id": "eb7f32625fa3b21cdbopjv"
+      "device_id": "...................."
       },
       'karin238': {
-      "ACCESS_ID": "yf4vxtcxqaej8tw4fukt",
-      "ACCESS_KEY": "dfb5a62742e3405f85387b8593c03a7f",
+      "ACCESS_ID": "....................",
+      "ACCESS_KEY": "....................",
       "API_ENDPOINT": "https://openapi.tuyaus.com/",
-      "device_id": "eb15370ddeb3b05df7jluz"
+      "device_id": "...................."
       },
       'karin239': {
-      "ACCESS_ID": "tgvqcqtfemjgku3v34wh",
-      "ACCESS_KEY": "d619ea2d449d4792b9fc2d810b06d1ee",
+      "ACCESS_ID": "....................",
+      "ACCESS_KEY": "....................",
       "API_ENDPOINT": "https://openapi.tuyaus.com/",
-      "device_id": "eb67cededa28c02b3frk7l"
+      "device_id": "...................."
       },
       'karin240':{
-      "ACCESS_ID": "gjwwvqhes8jdd44wdhmd",
-      "ACCESS_KEY": "268ebf65e80d470c860b0d39921efc2a",
+      "ACCESS_ID": "....................",
+      "ACCESS_KEY": "....................",
       "API_ENDPOINT":"https://openapi.tuyaus.com/",
-      "device_id": "ebaf2e3926d5a64d5bktsa"
+      "device_id": "...................."
       },
       }
